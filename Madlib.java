f27c710da7e0b50630b210584f3e543359d2efff
Alex2630 <42626222+Alex2630@users.noreply.github.com>
2018-09-06 00:30:17 -0500

    Initial commit

---
+++ Madlib.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Madlib {
+	public static void main(String[] args) {
+		String noun1;
+		String noun2;
+		String noun3;
+		String ADJ1;
+		String ADJ2;
+		String ADJ1;
+		String ADV2;
+		String ADV3;
+		Stirng Verb1;
+		String Verb2;
+		String Verb3;
+		
+		
+		
+		
+	Scanner inputDevice = new Scanner(system.in);
+
+	System.out.println("Please enter a noun");
+			noun1 = inputDevice.nextLine();
+			System.out.println("Noun 1: " + noun1);
+	System.out.println("Please enter a noun");	
+			noun2 = inputDevice.nextLine();
+			System.out.println("Noun 1: " + noun2);
+	System.out.println("Please enter a noun");
+			noun3 = inputDevice.nextLine();
+			System.out.println("Noun 1: " + noun3);
+	System.out.println("Please enter a verb ending in ing");
+			verb1 = inputDevice.nextLine();
+			System.out.println("verb 1: " + verb1);
+	System.out.println("Please enter a verb ending in ed");
+			verb2 = inputDevice.nextLine();
+			System.out.println("verb 2: " + verb2);
+	System.out.println("Please enter a verb");
+			verb3 = inputDevice.nextLine();
+			System.out.println("verb 3: " + verb3);
+	System.out.println("Please enter an adj");
+			adj1 = inputDevice.nextLine();
+			System.out.println("Adj 1: " + adj1);
+	System.out.println("Please enter an adj");
+			adj2 = inputDevice.nextLine();
+			System.out.println("Adj 2: " + adj2);
+	System.out.println("Please enter an adj");
+			adj3 = inputDevice.nextLine();
+			System.out.println("Adj 3: " + adj3);
+	System.out.println("Please enter an adv");
+			adv1 = inputDevice.nextLine();
+			System.out.println("Adv 1: " + adv1);
+	System.out.println("Please enter an adv");
+			adv2 = inputDevice.nextLine();
+			System.out.println("Adv 2: " + adv2);
+	System.out.println("Please enter an adv");
+			adv3 = inputDevice.nextLine();
+			System.out.println("Adv 3: " + adv3);
+	System.out.println("Once upon a time, there was a " + noun1 + " and it was the most " + adj1 + "est there ever was."
+	+ " Then, " + noun2  + " started " + verb1 + " rather" + adv1 + " as fast as it he could to " + noun1 + " remaining as " + adj1 + " as it could. " 
+	+ "All day and all night " + noun2 + " kept " + verb1 +" until " + noun1 + " became more " + adj3 + "."
+	+ " After " + noun2 + " became tired of " + verb1 + " " + noun1 + ", " + noun3 + " " + verb2 + " " + noun1 + " it was clear that " + noun2 + " had an error. "
+	+ "Now let this be a lesson to all. Just because " + noun1 + " " + verb3 + " rather " +  adv2 + " doesn't mean that things could go " + adv1 + ". "
+	+ "Who knows, " + noun3 + " could have " + verb3  + " " + adv3 + " " + noun1 +". That doesnt mean that the " + adj2 + " " + noun2 + " " + verb1 + " " + noun1+ " " + adv1 + " should be discounted either or viewed " + adv2 + ".");
+	}
+}
